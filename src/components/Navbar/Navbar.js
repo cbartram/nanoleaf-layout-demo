1786ca2e0e4ff6c0a1624912b41d5db36f54d592
cbartram <cbartram3@gmail.com>
2018-12-19 09:39:35 -0500

    Stylistic tweaks to demo

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Logo from '../../resources/images/logo.png';
+import './Navbar.css';
+
+export default class Navbar extends Component {
+  render() {
+      return (
+          <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm fixed">
+              <a className="navbar-brand" href="/">
+                  <img src={Logo} width="30" height="30" alt="Triangle Logo" />
+              </a>
+              <h5 className="my-0 mr-md-auto font-weight-normal">Nanoleaf Layout</h5>
+              <nav className="my-2 my-md-0 mr-md-3">
+                  <a className="p-2 text-dark" href="#about">About</a>
+                  <a className="p-2 text-dark" href="#examples">Examples</a>
+                  <a className="p-2 text-dark" href="#support">Support</a>
+                  <a className="p-2 text-dark" href="#github">Github</a>
+                  <a className="p-2 text-dark" href="#">Sponsor</a>
+              </nav>
+          </div>
+      )
+  }
+}
