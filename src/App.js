3cd2088aa8b3a3e05542f3bfbd094bc76babf00e
cbartram <cbartram3@gmail.com>
2018-12-19 13:34:39 -0500

    Added example section

--- src/App.js
+++ src/App.js
@@ -1,67 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import NanoleafLayout from 'nanoleaf-layout/lib/NanoleafLayout';
+import NanoleafData from './resources/data/nanoleafData';
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import RocketSVG from "./components/RocketSVG/RocketSVG";
 
 class App extends Component {
   constructor() {
       super();
 
       this.state = {
-          nanoleafData: {
-              numPanels: 10,
-              sideLength: 150,
-              positionData: [{
-                  panelId: 1,
-                  x: 100,
-                  y: 100,
-                  o: 60,
-                  color: '#6772E5'
-              }, {
-                  panelId: 2,
-                  x: 324,
-                  y: 56,
-                  o: 0,
-                  color: '#6772E5'
-              }, {
-                  panelId: 3,
-                  x: 249,
-                  y: -159,
-                  o: 60,
-                  color: '#6772E5'
-              }, {
-                  panelId: 4,
-                  x: 174,
-                  y: 56,
-                  o: 240,
-                  color: '#6772E5'
-              }, {
-                  panelId: 5,
-                  x: 324,
-                  y: -29,
-                  o: 60,
-                  color: '#6772E5'
-              }, {
-                  panelId: 7,
-                  x: 399,
-                  y: 99,
-                  o: 300,
-                  color: '#6772E5'
-              }, {
-                  panelId: 8,
-                  x: 174,
-                  y: -29,
-                  o: 60,
-                  color: '#6772E5'
-              }, {
-                  panelId: 10,
-                  x: 249,
-                  y: -73,
-                  o: 240,
-                  color: '#6772E5'
-              }]
-          }
+          data: NanoleafData
       }
   };
   render() {
@@ -75,7 +25,8 @@ class App extends Component {
                   <div className="d-flex justify-content-center">
                      <div className="nanoleaf-container-md">
                         <NanoleafLayout
-                            data={this.state.nanoleafData}
+                            style={{ display: 'block', margin: 'auto'}}
+                            data={this.state.data.panelSize['8']}
                             onClick={data => console.log('Clicked!', data)}
                             onHover={data => console.log('Hover', data)}
                             onExit={data => console.log('Exited Hover', data)}
@@ -85,16 +36,77 @@ class App extends Component {
               </div>
               {/* About Section */}
               <div className="row">
-                  <div className="col-md-5">
-                      <h1 className="display-5" id="about">About</h1>
+                  <div className="col-md-5 ml-3">
+                      <h1 className="display-5 right"  id="about">About</h1>
                       <p className="common-body-text">
-                          Stripe builds the most powerful and flexible tools for internet commerce. Whether you’re
-                          creating a subscription service, an on-demand marketplace, an e-commerce store, or a
-                          crowdfunding platform, Stripe’s meticulously designed APIs and unmatched functionality help
-                          you create the best possible product for your users. Millions of the world’s most innovative
-                          technology companies are scaling faster and more efficiently by building their businesses on
-                          Stripe.
+                          Nanoleaf Layout is the only package on NPM which takes your physical Nanoleaf layout and displays it in any 2D application.
+                          Nanoleaf Layout will take in the confusing X,Y coordinates and Orientation that comes from Nanoleaf's OpenAPI and converts
+                          it into a useful 2D graphic visual which you can use in your React application! Nanoleaf is a revolutionary smart lighting product which is fun and easy to use! It can be connected
+                          into different patterns with varying effects and colors. Ultimately this library helps open up new doors that allow users to intuitively interact with their
+                          Nanoleaf on a computer, phone, or tablet!
                       </p>
+                  </div>
+                  <div className="col-md-4 offset-md-1">
+                      <div className="nanoleaf-container-md">
+                          <NanoleafLayout
+                            data={this.state.data.panelSize['4']}
+                            strokeWidth={5}
+                          />
+                      </div>
+                  </div>
+              </div>
+              {/* Examples */}
+              <div className="row section-gray pt-3 pb-3">
+                  <div className="col-md-7 offset-md-2">
+                      <RocketSVG />
+                      <h1 className="display-5" id="example">Examples</h1>
+                      <p className="common-body-text center">
+                          This section details some code examples and the output they return to help you understand all the features
+                          Nanoleaf Layout has to offer. These examples include things like using props, handling errors, formatting data,
+                          and hooking into events to help your experience with Nanoleaf Layout blast off!
+                      </p>
+                  </div>
+              </div>
+              {/* Example */}
+              <div className="row section-gray">
+                  <div className="col-md-2 offset-md-2">
+                      <div className="nanoleaf-container-md">
+                        <NanoleafLayout data={this.state.data.panelSize[2]}/>
+                      </div>
+                  </div>
+                  <div className="col-md-4 offset-md-1">
+                      <h1 className="display-6">Bare Minimum</h1>
+                      <p className="common-body-text center">
+                          This is a bare minimum example of a simple Nanoleaf Layout React Component using the only required
+                          prop <code>data</code>.
+                      </p>
+                      <div className="code selected" data-language="javascript">
+                        <pre className=" numbered  language-javascript">
+                            <code className="language-javascript">
+                                <span className="token keyword">var</span> stripe <span className="token operator">=</span> <span className="token function">Stripe</span>
+                                <span className="token punctuation">(</span>
+                                <span className="token string publishable-key" style={{pointerEvents: 'none'}}>'pk_test_TYooMQauvdEDq54NiTphI7jx'</span>
+                                <span className="token punctuation">)</span>
+                                <span className="token punctuation">;</span>
+                                <span className="token keyword">var</span> elements
+                                <span className="token operator">=</span> stripe
+                                <span className="token punctuation">.</span>
+                                <span className="token property" />
+                                <span className="token function">elements</span>
+                                <span className="token punctuation">(</span>
+                                <span className="token punctuation">)</span>
+                                <span className="token punctuation">;</span>
+                            </code>
+                            <span className="line-numbers">
+                                <span></span>
+                                <span></span>
+                            </span>
+                            <span className="line-numbers">
+                                <span></span>
+                                <span></span>
+                            </span>
+                        </pre>
+                      </div>
                   </div>
               </div>
           </div>
---
+++ src/resources/data/nanoleafData.js
@@ -0,0 +1,196 @@
+const data = {
+    panelSize: {
+        8: {
+            numPanels: 10,
+            sideLength: 150,
+            positionData: [{
+                panelId: 1,
+                x: 100,
+                y: 100,
+                o: 60,
+                color: '#6772E5'
+            }, {
+                panelId: 2,
+                x: 324,
+                y: 56,
+                o: 0,
+                color: '#6772E5'
+            }, {
+                panelId: 3,
+                x: 249,
+                y: -159,
+                o: 60,
+                color: '#6772E5'
+            }, {
+                panelId: 4,
+                x: 174,
+                y: 56,
+                o: 240,
+                color: '#6772E5'
+            }, {
+                panelId: 5,
+                x: 324,
+                y: -29,
+                o: 60,
+                color: '#6772E5'
+            }, {
+                panelId: 7,
+                x: 399,
+                y: 99,
+                o: 300,
+                color: '#6772E5'
+            }, {
+                panelId: 8,
+                x: 174,
+                y: -29,
+                o: 60,
+                color: '#6772E5'
+            }, {
+                panelId: 10,
+                x: 249,
+                y: -73,
+                o: 240,
+                color: '#6772E5'
+            }]
+        },
+        10: {
+            numPanels: 10,
+            sideLength: 150,
+            positionData: [{
+                panelId: 1,
+                x: 100,
+                y: 100,
+                o: 60
+            }, {
+                panelId: 2,
+                x: 324,
+                y: 56,
+                o: 0
+            }, {
+                panelId: 3,
+                x: 249,
+                y: -159,
+                o: 60
+            }, {
+                panelId: 4,
+                x: 174,
+                y: 56,
+                o: 240
+            }, {
+                panelId: 5,
+                x: 324,
+                y: -29,
+                o: 60
+            }, {
+                panelId: 6,
+                x: -49,
+                y: 100,
+                o: 60
+            }, {
+                panelId: 7,
+                x: 399,
+                y: 99,
+                o: 300
+            }, {
+                panelId: 8,
+                x: 174,
+                y: -29,
+                o: 60
+            }, {
+                panelId: 9,
+                x: 25,
+                y: 56,
+                o: 120
+            }, {
+                panelId: 10,
+                x: 249,
+                y: -73,
+                o: 240
+            }]
+        },
+        6: {
+            numPanels: 10,
+            sideLength: 150,
+            positionData: [{
+                panelId: 1,
+                x: 100,
+                y: 100,
+                o: 0
+            }, {
+                panelId: 2,
+                x: 25,
+                y: -29,
+                o: 240
+            }, {
+                panelId: 3,
+                x: 174,
+                y: -29,
+                o: 120
+            }, {
+                panelId: 4,
+                x: 99,
+                y: 13,
+                o: 180
+            }, {
+                panelId: 5,
+                x: 99,
+                y: -159,
+                o: 0
+            }, {
+                panelId: 6,
+                x: 25,
+                y: -116,
+                o: 60
+            }]
+        },
+        4: {
+            numPanels: 10,
+            sideLength: 150,
+            positionData: [{
+                panelId: 1,
+                x: 100,
+                y: 100,
+                o: 0,
+                color: '#3ECF8E'
+            }, {
+                panelId: 2,
+                x: 25,
+                y: -29,
+                o: 240,
+                color: '#3ECF8E'
+            }, {
+                panelId: 3,
+                x: 174,
+                y: -29,
+                o: 120,
+                color: '#3ECF8E'
+            }, {
+                panelId: 4,
+                x: 99,
+                y: 13,
+                o: 180,
+                color: '#3ECF8E'
+            }]
+        },
+        2: {
+            numPanels: 2,
+            sideLength: 150,
+            positionData: [
+                {
+                    panelId: 107,
+                    x: -74,
+                    y: 43,
+                    o: 180
+                },
+                {
+                    panelId: 114,
+                    x: -149,
+                    y: 0,
+                    o: 360
+                }
+            ]
+        }
+    }
+};
+
+export default data;
